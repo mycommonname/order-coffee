19a2d031af989d98a50f4864a7503716e0d2b05e
mycommonname <you@example.com>
2026-04-21 18:53:24 +0500

    done

--- index.js
+++ index.js
@@ -114,8 +114,4 @@ closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-window.addEventListener('click', (e) => {
-    if (e.target === modal) modal.style.display = 'none';
-});
-
 document.querySelector('.remove-button').onclick = (e) => removeBeverage(e);
